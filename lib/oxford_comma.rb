a0e25ea25b4171ef85cb7257405e75b73811fa85
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 17:25:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,10 @@ def oxford_comma(array)
   elsif array.length == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array[-1] = "and #{array[-1]}"
+    array[array.index(-1)] = "and #{array[-1]}"
     new_array = 
   else
     array.join(", ")
   end
 end
+
