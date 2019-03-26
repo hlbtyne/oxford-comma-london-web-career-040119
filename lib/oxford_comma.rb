621953525ff368f02ec963e121d777dfda562aea
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 17:17:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length == 3
-      "#{array[0]}, #{array[1]}, and #{array[2]}"
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    
   else
     array.join(", ")
   end
